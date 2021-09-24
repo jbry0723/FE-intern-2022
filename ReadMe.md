f5b9192a6e853a5da06fdbebb18dc55c925aab3b
Justin Bryan <jbry0723@gmail.com>
2021-09-23 20:56:07 -0400

    added support for videos

--- ReadMe.md
+++ ReadMe.md
@@ -7,7 +7,7 @@ Includes date picker, loading spinner, and infinite scroll functionality.
 
 Uses react datepicker, date-fns, and styled components. 
 
-Designed to be responsive using breakpoints at 1024. Eliminated white-space on sides of smaller screens was a design decision to maximize use of small screens.
+Designed to be responsive using breakpoints at 1024px. Eliminated white-space on sides of smaller screens was a design decision to maximize use of small screens.
 
 Please send any questions to: jbry0723@gmail.com
 
--- src/Components/ImgContainer.js
+++ src/Components/ImgContainer.js
@@ -6,6 +6,15 @@ const ImgContainerDiv = styled.div`
   border-top-right-radius: 25px;
 
   vertical-align: middle;
+  iframe{
+    width: 1100px;
+    height:619px;
+    border-top-left-radius: 25px;
+    border-top-right-radius: 25px;
+    @media (max-width: 1024px) {
+      width: 100%;
+    }
+  }
   img {
     width: 1100px;
     border-top-left-radius: 25px;
@@ -18,6 +27,10 @@ const ImgContainerDiv = styled.div`
 
 const ImgContainer = (props) => {
   const { imgData } = props;
+  console.log(imgData)
+
+  if (imgData.media_type==="image")
+  {
 
   return (
     <ImgContainerDiv>
@@ -25,5 +38,17 @@ const ImgContainer = (props) => {
     </ImgContainerDiv>
   );
 };
+
+return (
+  <ImgContainerDiv>
+    <iframe title={imgData.title} src={imgData.url} alt={imgData.explanation}></iframe>
+  </ImgContainerDiv>
+);
+};
+
+
+
+
+
 
 export default ImgContainer;
